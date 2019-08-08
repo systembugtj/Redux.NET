48f36d8d80e0201809b899e633c9211d4f5b6bc2
systembug <albert_lee@hotmail.com>
2019-08-08 16:56:40 -0700

    refactor

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 nuget restore src/Redux.sln -verbosity detailed
 msbuild src/Redux.sln /p:Configuration=Release
 nunit-console src/Redux.Tests/bin/Release/Redux.Tests.dll
-nuget pack src/Redux/Redux.csproj -Prop Configuration=Release
-nuget add Redux.Net.3.0.0.nupkg -source ~/.nuget/packages
+nuget pack nuspec/Redux.NET.nuspec
+nuget add Redux.Net.3.0.0.nupkg -source ~/localpackages
 
 # nuget restore examples/todomvc/Redux.TodoMvc.sln -verbosity detailed
 # msbuild examples/todomvc/Redux.TodoMvc.sln /p:Configuration=Release
