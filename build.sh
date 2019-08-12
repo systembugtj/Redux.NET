c98a703b4da7341653c61bdc81fe3097dcf9304e
systembug <albert_lee@hotmail.com>
2019-08-12 09:40:53 -0700

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ nuget restore src/Redux.sln -verbosity detailed
 msbuild src/Redux.sln /p:Configuration=Release
 nunit-console src/Redux.Tests/bin/Release/Redux.Tests.dll
 nuget pack nuspec/Redux.NET.nuspec
-nuget add Redux.Net.3.0.0.nupkg -source ~/localpackages
+nuget add Redux.Net.3.0.1.nupkg -source ~/localpackages
 
 # nuget restore examples/todomvc/Redux.TodoMvc.sln -verbosity detailed
 # msbuild examples/todomvc/Redux.TodoMvc.sln /p:Configuration=Release
